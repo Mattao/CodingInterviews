2e6358524542b166e0f2c3e5ea008b82df4de3d7
matao <muxter.matao@gmail.com>
2017-11-16 14:56:25 +0800

    OPT improve readability

--- src/main/java/com/matao/Q7/QueueWith2Stacks.java
+++ src/main/java/com/matao/Q7/QueueWith2Stacks.java
@@ -8,40 +8,19 @@ import java.util.Stack;
  * Author: matao
  */
 public class QueueWith2Stacks {
-    Stack<Integer> stack1 = new Stack<Integer>();
-    Stack<Integer> stack2 = new Stack<Integer>();
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
 
     public void enqueque(int node) {
         stack1.push(node);
     }
 
     public int dequeue() {
-//        if (!stack2.isEmpty()) {
-//            return stack2.pop();
-//        } else {
-//            while(!stack1.isEmpty()) {
-//                stack2.push(stack1.pop());
-//            }
-//            return stack2.pop();
-//        }
-
         if (stack2.isEmpty()) {
             while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
             }
         }
         return stack2.pop();
-    }
-
-    public static void main(String[] args) {
-        QueueWith2Stacks q = new QueueWith2Stacks();
-        q.enqueque(2);
-        q.enqueque(3);
-        q.enqueque(4);
-        System.out.println(q.dequeue());
-        System.out.println(q.dequeue());
-        q.enqueque(5);
-        System.out.println(q.dequeue());
-        System.out.println(q.dequeue());
     }
 }
