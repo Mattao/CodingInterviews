4b585922d81e0519d13c89fe88e0e6b942f711b0
matao <muxter.matao@gmail.com>
2017-11-17 23:52:53 +0800

    TEST add unit test for reconstruct binary tree

---
+++ src/test/java/com/matao/Q6/ConstructBinaryTreeTest.java
@@ -0,0 +1,34 @@
+package com.matao.Q6;
+
+import com.matao.common.TreeNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ConstructBinaryTreeTest {
+
+    private ConstructBinaryTree constructBinaryTree;
+
+    @BeforeEach
+    public void setUp() {
+        constructBinaryTree = new ConstructBinaryTree();
+    }
+
+    /**
+     *  Normal Binary Tree
+     *              1
+     *           /     \
+     *          2       3
+     *         /       / \
+     *        4       5   6
+     *         \         /
+     *          7       8
+     */
+    @Test
+    public void testNormalBinaryTree() {
+        int[] pre = {1, 2, 4, 7, 3, 5, 6, 8};
+        int[] in = {4, 7, 2, 1, 5, 3, 8, 6};
+        TreeNode root = constructBinaryTree.reConstructBinaryTree(pre, in);
+        constructBinaryTree.preTraverse(root);
+        constructBinaryTree.inTraverse(root);
+    }
+}
