073aae3b6dfca09f58eea333639511d091f3276f
matao <muxter.matao@gmail.com>
2018-08-24 17:54:16 +0800

    feat(DuplicationInArray): add Q03 DuplicationInArray

---
+++ src/main/java/com/matao/Q03_DuplicationInArray.java
@@ -0,0 +1,154 @@
+package com.matao;
+
+import java.util.Arrays;
+
+/**
+ * Created by matao on 2018/8/24.
+ * <p>
+ * 一个长度为 n 的数组里所有数字都是在 0 - n-1 范围内。
+ * 数组中某些数字重复，但不知道有几个数字重复了，也不知道重复次数。
+ * 请找出数组中任意一个重复的数字。
+ * input: { 2， 3， 1， 0， 2， 5， 3 }
+ * output: 2 或 3
+ */
+public class Q03_DuplicationInArray {
+
+    /**
+     * 暴力求解
+     * 时间复杂度 O(n^2)
+     * 空间复杂度 O(1)
+     */
+    public static int findDuplication1(int[] data) {
+        if (data == null || data.length < 2) {
+            return -1;
+        }
+        for (int i = 0; i < data.length; i++) {
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[i] == data[j]) {
+                    return data[i];
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 先进行排序，再一趟遍历。 会改变原数组顺序
+     * 时间复杂度 O(nlogn)
+     * 空间复杂度 O(1)
+     */
+    public static int findDuplication2(int[] data) {
+        if (data == null || data.length < 2) {
+            return -1;
+        }
+        Arrays.sort(data);
+        int prev = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] == prev) {
+                return prev;
+            } else {
+                prev = data[i];
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 借助哈希原理，不会修改原始数据
+     * n 的数组里所有数字都是在 0 - n-1 范围内, 该哈希解构总长度n，能容纳所有数字,
+     * 一旦碰撞，则表示有重复数字。
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(n)
+     */
+    public static int findDuplication3(int[] data) {
+        if (data == null || data.length < 2) {
+            return -1;
+        }
+        int[] hashTable = new int[data.length];
+        for (int item : data) {
+            if (hashTable[item] == 1) {
+                return item;
+            }
+            hashTable[item] = 1;
+        }
+        return -1;
+    }
+
+    /**
+     * 因为数组长度 n, 且数字都是在 0 - n-1 范围内。
+     * 根据数字特点排序，会修改原始数据，
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(1)
+     */
+    public static int findDuplication4(int[] data) {
+        if (data == null || data.length < 2) {
+            return -1;
+        }
+        for (int i = 0; i < data.length; i++) {
+            while (data[i] != i) { // i 位置上的数data[i]与下标 i 不相等，则判断 data[i] 与 data[data[i]] 是否相等
+                if (data[i] == data[data[i]]) {
+                    return data[i];
+                }
+                int temp = data[i];
+                data[i] = data[data[i]];
+                data[data[i]] = temp;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 二分的思路计数，不修改原始数据，
+     * 时间复杂度 O(nlogn)
+     * 空间复杂度 O(1)
+     */
+    public static int findDuplication5(int[] data) {
+        if (data == null || data.length < 2) {
+            return -1;
+        }
+        int start = 0, end = data.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int count = countRange(data, start, mid);
+            if (start == end) {
+                if (count > 1) {
+                    return start;
+                } else {
+                    break;
+                }
+            }
+            if (count > (mid - start + 1)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    private static int countRange(int[] data, int start, int end) {
+        int count = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (start <= data[i] && end >= data[i])
+                count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] data1 = {2, 3, 1, 0, 2, 5, 3};
+        System.out.println(findDuplication1(data1));
+        System.out.println(findDuplication2(data1));
+        System.out.println(findDuplication3(data1));
+        System.out.println(findDuplication4(data1));
+        System.out.println(findDuplication5(data1));
+
+        int[] data2 = {2, 3, 1, 0, 4, 5, 5};
+        System.out.println(findDuplication1(data2));
+        System.out.println(findDuplication2(data2));
+        System.out.println(findDuplication3(data2));
+        System.out.println(findDuplication4(data2));
+        System.out.println(findDuplication5(data2));
+
+    }
+}
