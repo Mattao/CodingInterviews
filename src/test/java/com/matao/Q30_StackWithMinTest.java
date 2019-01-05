9e3186be984f78a5e06f456e1d324e60a113507a
matao <muxter.matao@gmail.com>
2019-01-06 01:11:56 +0800

    test(StackWithMin): add test cases for StackWithMin

--- src/test/java/com/matao/Q30_StackWithMinTest.java
+++ src/test/java/com/matao/Q30_StackWithMinTest.java
@@ -1,7 +1,33 @@
 package com.matao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
 /**
  * Created by matao on 2019-01-06.
  */
 public class Q30_StackWithMinTest {
+
+    private Q30_StackWithMin<Integer> stackWithMin;
+
+    @BeforeEach
+    void setUp() {
+        stackWithMin = new Q30_StackWithMin<>();
+    }
+
+    @Test
+    void testEmptyStack() {
+        Assertions.assertThrows(EmptyStackException.class, () -> stackWithMin.min());
+    }
+
+    @Test
+    void test() {
+        stackWithMin.push(1);
+        stackWithMin.push(3);
+        stackWithMin.push(-1);
+        Assertions.assertEquals(-1, stackWithMin.min().intValue());
+    }
 }
