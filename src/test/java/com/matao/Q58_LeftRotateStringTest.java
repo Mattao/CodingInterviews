c3a26b1bb4795cfdcaa8ea79b625ab206909f666
matao <muxter.matao@gmail.com>
2019-01-06 00:51:33 +0800

    test(LeftRotateString): add test cases for LeftRotateString

--- src/test/java/com/matao/Q58_LeftRotateStringTest.java
+++ src/test/java/com/matao/Q58_LeftRotateStringTest.java
@@ -1,7 +1,43 @@
 package com.matao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by matao on 2019-01-06.
  */
 public class Q58_LeftRotateStringTest {
+
+    private Q58_LeftRotateString leftRotateString;
+
+    @BeforeEach
+    void setUp() {
+        leftRotateString = new Q58_LeftRotateString();
+    }
+
+    @Test
+    void testNull() {
+        Assertions.assertNull(leftRotateString.leftRotate(null, 2));
+    }
+
+    @Test
+    void testEmptyString() {
+        Assertions.assertEquals("", leftRotateString.leftRotate("", 2));
+    }
+
+    @Test
+    void testNonRotateString() {
+        Assertions.assertEquals("abc", leftRotateString.leftRotate("abc", 0));
+    }
+
+    @Test
+    void testRotateString() {
+        Assertions.assertEquals("bca", leftRotateString.leftRotate("abc", 1));
+    }
+
+    @Test
+    void testOverRotateString() {
+        Assertions.assertEquals("cab", leftRotateString.leftRotate("abc", 5));
+    }
 }
