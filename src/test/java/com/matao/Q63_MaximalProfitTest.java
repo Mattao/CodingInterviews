b76776a9b87d7236c13ed6d55a645c86a131c9c5
matao <muxter.matao@gmail.com>
2019-01-06 02:09:09 +0800

    test(MaximalProfit): add test cases for MaximalProfit

--- src/test/java/com/matao/Q63_MaximalProfitTest.java
+++ src/test/java/com/matao/Q63_MaximalProfitTest.java
@@ -1,7 +1,30 @@
 package com.matao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by matao on 2019-01-06.
  */
 public class Q63_MaximalProfitTest {
+
+    private Q63_MaximalProfit maximalProfit;
+
+    @BeforeEach
+    void setUp() {
+        maximalProfit = new Q63_MaximalProfit();
+    }
+
+    @Test
+    void test1() {
+        int[] data = new int[]{9, 11, 8, 5, 7, 12, 16, 14};
+        Assertions.assertEquals(11, maximalProfit.maxDiff(data));
+    }
+
+    @Test
+    void test2() {
+        int[] data = new int[]{9, 8, 7, 6, 5, 4, 3, 1};
+        Assertions.assertEquals(-1, maximalProfit.maxDiff(data));
+    }
 }
