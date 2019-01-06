ad44cedbb998d609712879bfd3ee1abcd95cc886
matao <muxter.matao@gmail.com>
2019-01-06 17:57:01 +0800

    test(SortArrayForMinNumber): add test cases for SortArrayForMinNumber

--- src/test/java/com/matao/Q45_SortArrayForMinNumberTest.java
+++ src/test/java/com/matao/Q45_SortArrayForMinNumberTest.java
@@ -1,7 +1,24 @@
 package com.matao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by matao on 2019-01-06.
  */
 public class Q45_SortArrayForMinNumberTest {
+
+    private Q45_SortArrayForMinNumber sortArrayForMinNumber;
+
+    @BeforeEach
+    void setUp() {
+        sortArrayForMinNumber = new Q45_SortArrayForMinNumber();
+    }
+
+    @Test
+    void test() {
+        int[] data = {3, 32, 321};
+        Assertions.assertEquals(321323, sortArrayForMinNumber.minNumber(data));
+    }
 }
