4c5f154842b3343c58fe342836f4d82ee1407f0d
matao <muxter.matao@gmail.com>
2019-01-06 17:46:56 +0800

    test(KLeastNumbers): add test cases for KLeastNumbers

--- src/test/java/com/matao/Q40_KLeastNumbersTest.java
+++ src/test/java/com/matao/Q40_KLeastNumbersTest.java
@@ -1,7 +1,41 @@
 package com.matao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by matao on 2019-01-06.
  */
 public class Q40_KLeastNumbersTest {
+
+    private Q40_KLeastNumbers kLeastNumbers;
+
+    @BeforeEach
+    void setUp() {
+        kLeastNumbers = new Q40_KLeastNumbers();
+    }
+
+    @Test
+    void testNull() {
+        Assertions.assertEquals(Integer.MIN_VALUE, kLeastNumbers.getLeastNumbers(null, 1));
+    }
+
+    @Test
+    void testEmptyArray() {
+        int[] a = {};
+        Assertions.assertEquals(Integer.MIN_VALUE, kLeastNumbers.getLeastNumbers(a, 1));
+    }
+
+    @Test
+    void test1() {
+        int[] array = {4, 5, 1, 6, 2, 7, 3, 8};
+        Assertions.assertEquals(4, kLeastNumbers.getLeastNumbers(array, 4));
+    }
+
+    @Test
+    void test2() {
+        int[] array = {6, 1, 3, 5, 4, 2};
+        Assertions.assertEquals(5, kLeastNumbers.getLeastNumbers(array, 5));
+    }
 }
